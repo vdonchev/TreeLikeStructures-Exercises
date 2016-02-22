1fca4a7d7d2b55d1d24669bce0f9b3e6b4bda8c7
vdonchev <ya.5164646777752015@yandex.ru>
2016-02-22 15:56:38 +0200

    init

---
+++ Binary-Tree/BinaryTree.cs
@@ -0,0 +1,68 @@
+﻿namespace Trees
+{
+    using System;
+
+    public class BinaryTree<T>
+    {
+        public BinaryTree(
+            T value, 
+            BinaryTree<T> leftChild = null, 
+            BinaryTree<T> rightChild = null)
+        {
+            this.Value = value;
+            this.LeftChild = leftChild;
+            this.RightChild = rightChild;
+        }
+
+        public T Value { get; private set; }
+
+        public BinaryTree<T> LeftChild { get; private set; }
+
+        public BinaryTree<T> RightChild { get; private set; }
+
+        public void PrintIndentedPreOrder(int indent = 0)
+        {
+            Console.WriteLine(new string(' ', indent * 2) + this.Value);
+
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.PrintIndentedPreOrder(indent + 1);
+            }
+
+            if (this.RightChild != null)
+            {
+                this.RightChild.PrintIndentedPreOrder(indent + 1);
+            }
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.EachInOrder(action);
+            }
+
+            action(this.Value);
+
+            if (this.RightChild != null)
+            {
+                this.RightChild.EachInOrder(action);
+            }
+        }
+
+        public void EachPostOrder(Action<T> action)
+        {
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.EachPostOrder(action);
+            }
+
+            if (this.RightChild != null)
+            {
+                this.RightChild.EachPostOrder(action);
+            }
+
+            action(this.Value);
+        }
+    }
+}
---
+++ Binary-Tree/Tree.cs
@@ -0,0 +1,43 @@
+﻿namespace Trees
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Tree<T>
+    {
+        public Tree(
+            T value, 
+            params Tree<T>[] children)
+        {
+            this.Value = value;
+
+            this.Children = new List<Tree<T>>();
+            foreach (var child in children)
+            {
+                this.Children.Add(child);
+            }
+        }
+
+        public T Value { get; private set; }
+
+        public IList<Tree<T>> Children { get; private set; }
+
+        public void Print(int indent = 0)
+        {
+            Console.WriteLine(new string(' ', indent * 2) + this.Value);
+            foreach (var child in this.Children)
+            {
+                child.Print(indent + 1);
+            }
+        }
+
+        public void Each(Action<T> action)
+        {
+            action(this.Value);
+            foreach (var child in this.Children)
+            {
+                child.Each(action);
+            }
+        }
+    }
+}
